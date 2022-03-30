3ac3bf4b6fa8daadab1ebb5b0fed1e9496d7cdbb
cmstechsutantra <sumit.deore@techsutantra.com>
2022-03-30 22:19:47 +0530

    make it better

--- src/models/company.model.js
+++ src/models/company.model.js
@@ -60,7 +60,7 @@ Companys.createCompany = (companyReqData, result) =>{
             result(null, err);
         }else{
             console.log('Error while fetching companys by id',res);
-
+            if(res!=null)
             result(null, res);
             
             // if(res!=null){
