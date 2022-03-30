9d00472dc07a6e48caf90f12c4e64781945e5d8f
cmstechsutantra <sumit.deore@techsutantra.com>
2022-03-30 23:37:43 +0530

    make it better

--- src/models/company.model.js
+++ src/models/company.model.js
@@ -71,7 +71,7 @@ Companys.createCompany = (companyReqData, result) =>{
                     }
                 });
             }else{
-                result(null, res.length, err);
+                result(null, err);
             }
         }
     })
