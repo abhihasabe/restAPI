44b6f70cd2c8a044df07e0d5408dc68a57c2819a
cmstechsutantra <sumit.deore@techsutantra.com>
2022-03-30 10:39:23 +0530

    make it better

--- src/routes/base.route.js
+++ src/routes/base.route.js
@@ -12,4 +12,7 @@ router.get('/getCityByCountryID/:id',companysController.getCityByCountryID);
 // get all companys Type
 router.get('/getcompanytypes', companysController.getCompanysTypeList);
 
+// get all User Type
+router.get('/getusertypes', companysController.getUserTypeList);
+
 module.exports = router;
